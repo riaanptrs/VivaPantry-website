bc2276279cb34ea7f9565922f5c66aaf6f15c702
riaanptrs <riaanptrs@gmail.com>
2026-06-21 06:07:26 -0300

    Deploy Phase 1 static VivaPantry site

---
+++ script.js
@@ -0,0 +1,9 @@
+document.documentElement.classList.add('js-enabled');
+document.querySelectorAll('a[href^="#"]').forEach(function (link) {
+  link.addEventListener('click', function (event) {
+    var target = document.querySelector(link.getAttribute('href'));
+    if (!target) return;
+    event.preventDefault();
+    target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  });
+});
